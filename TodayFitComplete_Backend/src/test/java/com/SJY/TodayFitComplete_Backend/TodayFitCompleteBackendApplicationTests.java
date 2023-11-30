08f01f7d383485b8a6302ad670ef7831a28c6b59
joon6093 <118044367+joon6093@users.noreply.github.com>
2023-11-30 18:38:42 +0900

    [INIT] 초기 환경 설정

---
+++ TodayFitComplete_Backend/src/test/java/com/SJY/TodayFitComplete_Backend/TodayFitCompleteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SJY.TodayFitComplete_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayFitCompleteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
