53ca7cb701e5590104140c8c63ff9954a9932f7c
Jeyong <joon6093@naver.com>
2024-01-16 05:54:46 +0900

    [UPDATE] 사용자에게 보여주는 이미지 및 동영상을 캐시를 사용하도록 수정

--- TodayFitComplete_Backend/src/main/java/com/SJY/TodayFitComplete_Backend/config/WebConfig.java
+++ TodayFitComplete_Backend/src/main/java/com/SJY/TodayFitComplete_Backend/config/WebConfig.java
@@ -1,17 +1,23 @@
 package com.SJY.TodayFitComplete_Backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${project.folderPath}")
+    private String location;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
-                .addResourceLocations("file:/app/files/");
+                .addResourceLocations("file:" + location).setCacheControl(CacheControl.maxAge(Duration.ofHours(1L)).cachePublic());
     }
 }
